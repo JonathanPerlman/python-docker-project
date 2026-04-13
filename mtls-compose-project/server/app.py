deb9bf02aa32b58ab027c7bb3f4509aedba2593b
Your Name <your-email@example.com>
2026-04-13 15:08:55 +0300

    add prometheus and grapana into to the project

--- mtls-compose-project/server/app.py
+++ mtls-compose-project/server/app.py
@@ -1,27 +1,36 @@
 import http.server
 import ssl
 import socketserver
+from prometheus_client import start_http_server, Counter
 
-PORT = 8000
+MTLS_PORT = 8000
+METRICS_PORT = 9000 
+
+REQUESTS_COUNTER = Counter('mtls_verified_requests_total', 'Total number of verified mTLS requests')
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+
+        REQUESTS_COUNTER.inc()
+        
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(b"Hello! mTLS Connection Verified.")
         print("Log: Connection verified with Client Certificate!")
 
-httpd = socketserver.TCPServer(("0.0.0.0", PORT), MyHandler)
+httpd = socketserver.TCPServer(("0.0.0.0", MTLS_PORT), MyHandler)
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(certfile="server.crt", keyfile="server.key")
-
 context.verify_mode = ssl.CERT_REQUIRED
 context.load_verify_locations(cafile="client.crt")
 
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
-print(f"Server running on port {PORT} with Strict mTLS compose...")
-httpd.serve_forever()
-  
+if __name__ == "__main__":
+    start_http_server(METRICS_PORT)
+    print(f"Metrics available at http://localhost:{METRICS_PORT}")
+    
+    print(f"Server running on port {MTLS_PORT} with Strict mTLS...")
+    httpd.serve_forever()
